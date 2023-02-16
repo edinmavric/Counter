3ecf14f4edce992364beca2248a2e787f68fec08
edinmavric <edinmavric10@gmail.com>
2023-02-16 20:14:53 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let numberCounter = document.getElementById('number-counter');
+let buttonAdd = document.querySelector('.btn-add');
+let buttonSubstract = document.querySelector('.btn-substract');
+let buttonReset = document.querySelector('.btn-reset');
+let counter = 0;
+
+buttonAdd.addEventListener("click", () => {
+    counter++;
+    numberCounter.innerHTML = counter;
+});
+  
+buttonSubstract.addEventListener("click", () => {
+    counter--;
+    numberCounter.innerHTML = counter;
+});
+
+buttonReset.addEventListener('click', () => {
+    counter = 0;
+    numberCounter.innerHTML = counter;
+});
