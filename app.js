44a92f93aa20ae1ae76a3804e2877f91e4d95e5d
edinmavric <edinmavric10@gmail.com>
2023-02-17 21:19:40 +0100

    added multiply and divide buttons

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let numberCounter = document.getElementById("number-counter");
 let buttonAdd = document.querySelector(".btn-add");
 let buttonSubstract = document.querySelector(".btn-substract");
+let buttonMultiply = document.querySelector(".btn-multiply");
+let buttonDivide = document.querySelector(".btn-divide");
 let buttonReset = document.querySelector(".btn-reset");
 let counter = 0;
 
 const colorChanger = () => {
   if (numberCounter.textContent == 0) {
-    numberCounter.style.color = "#273361"; 
+    numberCounter.style.color = "#273361";
   } else if (numberCounter.textContent > 0) {
     numberCounter.style.color = "#75AD3D";
   } else if (numberCounter.textContent < 0) {
@@ -14,16 +16,45 @@ const colorChanger = () => {
   }
 };
 
+const checkIfNaN = (num) => {
+  if (isNaN(num)) {
+    alert("Entered value is not a number, try again!");
+    return true;
+  }
+};
+
+const checkDecimal = () => {
+  if (Number.isInteger(counter)) {
+    numberCounter.innerHTML = counter;
+  } else {
+    numberCounter.innerHTML = counter.toFixed(2);
+  }
+};
+
+buttonMultiply.addEventListener("click", () => {
+  let num = +prompt("Enter number to multiply current counter number");
+  if (checkIfNaN(num)) return;
+  counter *= num;
+  checkDecimal();
+});
+
+buttonDivide.addEventListener("click", () => {
+  let num = +prompt("Enter number to divide current counter number");
+  if (checkIfNaN(num)) return;
+  counter /= num;
+  checkDecimal();
+});
+
 buttonAdd.addEventListener("click", () => {
   counter++;
-  numberCounter.innerHTML = counter;
   colorChanger();
+  checkDecimal();
 });
 
 buttonSubstract.addEventListener("click", () => {
   counter--;
-  numberCounter.innerHTML = counter;
   colorChanger();
+  checkDecimal();
 });
 
 buttonReset.addEventListener("click", () => {
