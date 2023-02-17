4b15430236bed815391c7beb12bdbb79cfd95e77
edinmavric <edinmavric10@gmail.com>
2023-02-17 20:29:28 +0100

    finished counter and shortened code

--- app.js
+++ app.js
@@ -4,26 +4,30 @@ let buttonSubstract = document.querySelector(".btn-substract");
 let buttonReset = document.querySelector(".btn-reset");
 let counter = 0;
 
+const colorChanger = () => {
+  if (numberCounter.textContent == 0) {
+    numberCounter.style.color = "#273361"; 
+  } else if (numberCounter.textContent > 0) {
+    numberCounter.style.color = "#75AD3D";
+  } else if (numberCounter.textContent < 0) {
+    numberCounter.style.color = "#AD1900";
+  }
+};
+
 buttonAdd.addEventListener("click", () => {
   counter++;
   numberCounter.innerHTML = counter;
-  if (numberCounter.textContent > 0) {
-    numberCounter.style.color = '#75AD3D'
-  }
+  colorChanger();
 });
 
 buttonSubstract.addEventListener("click", () => {
   counter--;
   numberCounter.innerHTML = counter;
-  if (numberCounter.textContent < 0) {
-    numberCounter.style.color = '#AD1900'
-  }
+  colorChanger();
 });
 
 buttonReset.addEventListener("click", () => {
   counter = 0;
   numberCounter.innerHTML = counter;
-  if (numberCounter.textContent == 0) {
-    numberCounter.style.color = '#273361' 
-  }
+  colorChanger();
 });
